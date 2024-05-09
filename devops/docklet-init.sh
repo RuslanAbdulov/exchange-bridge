6538d1f84b28e175bf78695133464a7dbd9f4a5b
ruslan <ratioff@list.ru>
2024-05-10 02:24:26 +0400

    script update

--- devops/docklet-init.sh
+++ devops/docklet-init.sh
@@ -6,7 +6,10 @@ cd exchange-bridge/
 #./gradlew tasks
 #./gradlew clean build --no-daemon
 
-#ssh root@165.227.48.202
+#ssh root@152.42.128.231
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 9000
+
 mkdir ~/app
-scp build/libs/exchange-bridge-0.0.1-SNAPSHOT.jar root@165.227.48.202:/root/app/
-java -jar ~/app/exchange-bridge-0.0.1-SNAPSHOT.jar --spring.application.json=
+scp build/libs/exchange-bridge-0.0.1-SNAPSHOT.jar root@152.42.128.231:/root/app/exchange-bridge.jar
+cd app
+
